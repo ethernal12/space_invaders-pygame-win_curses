985720818fc39ee19f693f5636036d1749056e87
DanijelK <korbar41@gmail.com>
2023-02-20 16:39:46 +0100

    Dodajanje requirements.txt.

--- src/app/GUI.py
+++ src/app/GUI.py
@@ -2,6 +2,13 @@ from src.app._app import App
 
 
 class Gui(App):
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+        self.dx: int = 0
+        self.dy: int = 0
+        self.windowSurface: Optional[pygame.Surface] = None
+        self.clock = pygame.time.Clock()
     def inicializacija_igre(self):
         pass
 
