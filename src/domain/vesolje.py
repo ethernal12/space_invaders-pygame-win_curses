84a2eafd812a98ead59f4f3031901569bb2e9b3c
DanijelK <korbar41@gmail.com>
2023-02-25 16:28:12 +0100

    Dodajanje vesolje test in github workflow.

--- src/domain/vesolje.py
+++ src/domain/vesolje.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from domain.ladja import Ladja
+from src.domain.ladja import Ladja
 
 from src.domain.vesoljci import Vesoljci
 
@@ -15,6 +15,7 @@ class Vesolje:
         self.ladja = Ladja(x=0.5, y=1, velikost_x=0.1, velikost_y=0.1, hitrost=0.003)
 
     def omejitev_ladje(self):
+
         if self.ladja.x >= 1:
             self.ladja.x = 1
         elif self.ladja.x <= 0:
